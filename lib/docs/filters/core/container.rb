7aba1f6c07f2aa5efc3b05bbecad6d0c3abc1d72
Alex <alex@brewingcode.net>
2017-11-24 13:33:54 -0800

    Output html that raises errors

--- lib/docs/filters/core/container.rb
+++ lib/docs/filters/core/container.rb
@@ -4,12 +4,18 @@ module Docs
   class ContainerFilter < Filter
     class ContainerNotFound < StandardError; end
 
+    def whine(container)
+      html = doc.to_html
+      puts html.unpack('U'*html.length).collect {|x| x.to_s 16}.join ' '
+      raise(ContainerNotFound, "element '#{container}' could not be found in the document, url=#{current_url}")
+    end
+
     def call
       container = context[:container]
       container = container.call self if container.is_a? Proc
 
       if container
-        doc.at_css(container) || raise(ContainerNotFound, "element '#{container}' could not be found in the document, url=#{current_url}")
+        doc.at_css(container) || whine(container)
       elsif doc.name == 'document'
         doc.at_css('body')
       else
